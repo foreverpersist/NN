102b5e429a5791abb9075ad18928915b59c29b18
Joy Allen <1554706732@qq.com>
2018-04-12 16:28:22 +0800

    Fix MSE.

--- loss.py
+++ loss.py
@@ -1,31 +1,32 @@
-
+import numpy as np
 ''' Some kinds of Loss Functions
 '''
 
 class MSE:
-	''' Mean Sqaure Error:
-	        C = sum((y-a)^2)
-	'''
-	def loss(self, batchA, batchY):
-		batchD = batchA - batchY
-		return np.sqaure(batchD)
+    ''' Mean Sqaure Error:
+            C = sum((y-a)^2)
+    '''
+    def loss(self, batchA, batchY):
+        batchD = batchA - batchY
+        square = np.square(batchD)
+        return np.sum(square)
 
-	''' Delta: 
-	        @C/@aL
-	'''
-	def delta(self, batchA, batchY):
-		return 2 * (batchA - batchY)
+    ''' Delta: 
+            @C/@aL
+    '''
+    def delta(self, batchA, batchY):
+        return 2 * (batchA - batchY)
 
 
 class CE:
-	''' Cross Entropy:
-	        C = yloga + (1-y)log(1-a)
-	'''
-	def loss(self, batchA, batchY):
-		return
+    ''' Cross Entropy:
+            C = yloga + (1-y)log(1-a)
+    '''
+    def loss(self, batchA, batchY):
+        return
 
-	def delta(self, batchA, batchY):
-		return
+    def delta(self, batchA, batchY):
+        return
 
 mse = MSE()
 ce = CE()
